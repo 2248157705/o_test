2f668d0aba80834c99a4ff5402b978e80ed3a335
chenshiyi <chenshiyi@chenshiyideMacBook-Air.local>
2025-03-23 08:12:43 +0800

    app

---
+++ app/build-app/android_debug/config.js
@@ -0,0 +1,32 @@
+export const releaseConfig = {
+    applicationId: "vip.taogehao",
+    WX_APPID: "wxa95eac8d2c26450a",
+    WX_SECRET: "54ec8a651cef6cfdb4a2ff985f2b9396",
+    UMENG_APPKEY: "66599998cac2a664de401dba",
+    PACKAGE_NAME: "vip.taogehao",
+    DCLOUD_APPKEY: "65acb97edccda627b0e0a1c9c25fbac9",
+    APP_SCHEME: "taogehao",
+    APP_SCHEME_HOST: "app",
+    APP_HTTP_SCHEME: "https",
+    APP_HTTP_SCHEME_HOST: "web.sh28.vip",
+    APP_HTTP_SCHEM_PATHPREFIX: "/app",
+    CHANNEL_VALUE: ""
+}
+
+export const debugConfig = {
+    applicationId: "vip.taogehaotest",
+    WX_APPID: "wxe4673f48bb76f8c8",
+    WX_SECRET: "4d40b78dd6e1cc2e2c76515ca18bedb7",
+    UMENG_APPKEY: "66599998cac2a664de401dba",
+    PACKAGE_NAME: "vip.taogehaotest",
+    DCLOUD_APPKEY: "e0c49f2dd2a80a1a25352076078c7e00",
+    APP_SCHEME: "taogehaotest",
+    APP_SCHEME_HOST: "app",
+    APP_HTTP_SCHEME: "https",
+    APP_HTTP_SCHEME_HOST: "web.sh28.vip",
+    APP_HTTP_SCHEM_PATHPREFIX: "/app",
+    VOLCENGINE_APPID: "",
+    VOLCENGINE_REPORT_URL: "https://gator.volces.com",
+    CHANNEL_VALUE: ""
+
+}
